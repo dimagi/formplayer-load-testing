8575252b1dd82985afa82b9bd7fbf23a79079d01
Ben <rudolphben@gmail.com>
2017-02-20 10:45:45 +0200

    fix env variable

--- scripts/referrals.sh
+++ scripts/referrals.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-env WORKFLOW=task_list.referrals locust --host=http://10.162.36.231:8181 NikshayCaseListLocust
+env WORKFLOW=referrals locust --host=http://10.162.36.231:8181 NikshayCaseListLocust
